b00cd77681928f9aed304af65bbf4046128c7d6f
Sandra Tatarevićová <sandra.tatarevicova@ackee.cz>
2019-10-15 07:56:21 +0200

    Upgrade to Terraform 0.12. See RM #35486.

--- variables.tf
+++ variables.tf
@@ -1,18 +1,30 @@
-variable "project" {}
-variable "zone" {}
-variable "instance_name" {}
-variable "cluster_ipv4_cidr" {}
+variable "project" {
+}
+
+variable "zone" {
+}
+
+variable "instance_name" {
+}
+
+variable "cluster_ipv4_cidr" {
+}
 
 variable "node_count" {
   default = "1"
 }
+
 variable "machine_type" {
   default = "n1-standard-1"
 }
-variable "raw_image_source" {}
+
+variable "raw_image_source" {
+}
+
 variable "rs" {
   default = "none"
 }
+
 variable "data_disk_gb" {
   default = "30"
 }
@@ -20,3 +32,4 @@ variable "data_disk_gb" {
 variable "data_disk_type" {
   default = "pd-standard"
 }
+
